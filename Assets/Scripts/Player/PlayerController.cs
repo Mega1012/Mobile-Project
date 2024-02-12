48858245715a0d00f8da20c710a2a89844a0fc5d
Mega1012 <felipe.fsp303@gmail.com>
2024-02-12 10:53:15 -0300

    TODO O INICIO KKKKKKKKKKKKKKKKKK

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //publics
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+    
     public float speed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public string tagToCheckEnemy = "Enemy";
+    public string tagToCheckFinishLine = "FinishLine";
+
+    public GameObject endScreen;
+
+    public GameObject startScreen;
+
+    //privates
+    private bool _canRun;
+    private Vector3 _pos;
+
+    
 
     // Update is called once per frame
     void Update()
     {
+        if(!_canRun) return;
+
+        var _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+        
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == tagToCheckEnemy)
+        {
+            EndGame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == tagToCheckFinishLine)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        _canRun = false;
+        endScreen.SetActive(true);
+    }
+
+    public void StartToRun()
+    {
+        _canRun = true;
+        startScreen.SetActive(false);
     }
 }
